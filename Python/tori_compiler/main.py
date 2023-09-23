2e6b3305b4953e53d21788bf5324cbb269d2fc9f
yamabato <harutakutsukawa0601@gmail.com>
2023-09-24 01:26:14 +0900

    ref: 小数の字句解析

--- Python/tori_compiler/main.py
+++ Python/tori_compiler/main.py
@@ -1,7 +1,7 @@
 from tr_lexer import tr_lexer
 
 program = """
-1.23"""
+123.456"""
 
 tokens = tr_lexer(program)
 print(tokens)
--- Python/tori_compiler/tr_lexer.py
+++ Python/tori_compiler/tr_lexer.py
@@ -8,11 +8,13 @@ WHITESPACE = " \t\n"
 # ---
 # ヘルパー関数
 
-def next_char(program, n):
-  if len(program) <= n+1: return n+1, ""
-  
+def get_current_char(program, n):
+  if len(program) <= n: return ""
+  return program[n]
+
+def get_next_char(program, n):
   n += 1
-  return n, program[n]
+  return n, get_current_char(program, n) 
 
 def check_char_type(char):
   if char == "": return None
@@ -22,15 +24,34 @@ def check_char_type(char):
 # ---
 # 各種リテラル読み込み
 
-def read_numerical_literal(program, n):
+def read_digits(program, n):
   token_value = ""
-  c = program[n]
+  c = get_current_char(program, n)
+
   while check_char_type(c) == "NUMBER": 
     token_value += c
-    n, c = next_char(program, n)
-  
+    n, c = get_next_char(program, n)
+
   kind = TR_Token_Kind.INT
   return kind, token_value, n
+
+def read_numerical_literal(program, n):
+  token_value = ""
+  kind = TR_Token_Kind.INT
+
+  _, digits, n = read_digits(program, n)
+  token_value = digits
+
+  c = get_current_char(program, n)
+  if c == ".":
+    kind = TR_Token_Kind.DEC
+    token_value += "."
+    n, c = get_next_char(program, n)
+
+  _, digits, n = read_digits(program, n)
+  token_value = digits
+  
+  return kind, digits, n
 
 # ---
 
@@ -41,7 +62,7 @@ def tr_lexer(program):
   n = 0
   
   while n < program_length:
-    c = program[n]
+    c = get_current_char(program, n)
 
     # Token_Kind.INT, Token_Kind.DEC
     if c in NUMBER or c == ".":
@@ -49,6 +70,8 @@ def tr_lexer(program):
       token = TR_Token(kind, value)
       tokens.append(token)
 
-    n, c = next_char(program, n)
+    n, c = get_next_char(program, n)
 
   return tokens 
+
+# ---
