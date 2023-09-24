2f984e0efe68ad309a9439ce1edcf68fb9cd0cf1
yamabato <harutakutsukawa0601@gmail.com>
2023-09-25 02:37:10 +0900

    test: 比較演算の構文解析についてのテストを追加

--- Python/tests/parser_test_util.py
+++ Python/tests/parser_test_util.py
@@ -40,6 +40,9 @@ def expand_tree(tree):
   elif node_type == TR_Node_Kind.ASSIGN_ADD:
     expanded += f"ASSIGN_ADD({expand_tree(tree.name)}, {expand_tree(tree.expr)})"
 
+  elif node_type == TR_Node_Kind.EQUAL:
+    expanded += f"EQUAL({expand_tree(tree.left)}, {expand_tree(tree.right)})"
+
   elif node_type == TR_Node_Kind.CALL:
     name = expand_tree(tree.name)
     args = ", ".join([expand_tree(arg) for arg in tree.args])
---
+++ Python/tests/test_parser_compare.py
@@ -0,0 +1,19 @@
+from tori_test import *
+from parser_test_util import *
+
+# ---
+# 比較に関するテスト
+
+def test_parser_EQUAL_01():
+  expanded_code = get_expanded_code("a==1")
+  assert expanded_code == "EQUAL(VAR(a), INT(1))" 
+
+def test_parser_EQUAL_02():
+  expanded_code = get_expanded_code("a==1+2")
+  assert expanded_code == "EQUAL(VAR(a), ADD(INT(1), INT(2)))" 
+
+def test_parser_EQUAL_03():
+  expanded_code = get_expanded_code("a**2==1+2")
+  assert expanded_code == "EQUAL(POWER(VAR(a), INT(2)), ADD(INT(1), INT(2)))" 
+
+# ---
