d0faaea2bcf4a837a012755ca3e57223112bd17a
yamabato <harutakutsukawa0601@gmail.com>
2023-09-25 16:01:09 +0900

    test: テスト関数において文字列の展開を可能にする

--- Python/tests/parser_test_util.py
+++ Python/tests/parser_test_util.py
@@ -22,6 +22,9 @@ def expand_tree(tree):
   elif node_type == TR_Node_Kind.DEC:
     expanded += f"DEC({tree.value})"
 
+  elif node_type == TR_Node_Kind.STR:
+    expanded += f"STR({tree.value})"
+
   elif node_type == TR_Node_Kind.VAR:
     expanded += f"VAR({tree.value})"
 
