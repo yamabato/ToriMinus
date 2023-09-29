951105103c08690f7d9bfb53d2881150a59ff42a
yamabato <harutakutsukawa0601@gmail.com>
2023-09-29 11:39:22 +0900

    test: 単項演算子の評価に関するテスト

---
+++ Python/tests/test_eval_unary.py
@@ -0,0 +1,23 @@
+from tori_test import *
+from eval_test_util import *
+
+# ---
+# 代入演算子に関するテスト
+
+def test_eval_UNARY_01():
+  ret = run_tori_minus_test_code("+10;")
+  assert ret == 10 
+
+def test_eval_UNARY_02():
+  ret = run_tori_minus_test_code("-10;")
+  assert ret == -10 
+
+def test_eval_UNARY_03():
+  ret = run_tori_minus_test_code("-(10+2*3);")
+  assert ret == -60 
+
+def test_eval_UNARY_04():
+  ret = run_tori_minus_test_code("a=12; -a;")
+  assert ret == -12 
+
+# ---
