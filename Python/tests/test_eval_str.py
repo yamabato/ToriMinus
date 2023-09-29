d409994ef7e3c6efb7e91eaa3410e70199b3b1bc
yamabato <harutakutsukawa0601@gmail.com>
2023-09-29 09:42:24 +0900

    test: 文字列の評価に関するテスト

---
+++ Python/tests/test_eval_str.py
@@ -0,0 +1,19 @@
+from tori_test import *
+from eval_test_util import *
+
+# ---
+# 文字列に関するテスト
+
+def test_eval_STR_01():
+  ret = run_tori_minus_test_code("\"str\"")
+  assert ret == "str" 
+
+def test_eval_STR_02():
+  ret = run_tori_minus_test_code("\"str\" + \"aaa\"")
+  assert ret == "straaa"
+
+def test_eval_STR_03():
+  ret = run_tori_minus_test_code("\"str\" * 12")
+  assert ret == "strstrstrstrstrstrstrstrstrstrstrstr"
+
+# ---
