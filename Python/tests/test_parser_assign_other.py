bb827f763745b36626dc73b95a59343a77c0b95a
yamabato <harutakutsukawa0601@gmail.com>
2023-09-29 11:03:04 +0900

    test: 代入演算子のテストケースを修正

--- Python/tests/test_parser_assign_other.py
+++ Python/tests/test_parser_assign_other.py
@@ -6,54 +6,50 @@ from parser_test_util import *
 
 def test_parser_ASSIGN_OTHER_01():
   expanded_code = get_expanded_code("a+=1;")
-  assert expanded_code == "ASSIGN_ADD(VAR(a), INT(1))" 
+  assert expanded_code == "ASSIGN(VAR(a), ADD(VAR(a), INT(1)))" 
 
 def test_parser_ASSIGN_OTHER_02():
   expanded_code = get_expanded_code("a-=1;")
-  assert expanded_code == "ASSIGN_SUB(VAR(a), INT(1))" 
+  assert expanded_code == "ASSIGN(VAR(a), SUB(VAR(a), INT(1)))" 
 
 def test_parser_ASSIGN_OTHER_03():
   expanded_code = get_expanded_code("a*=1;")
-  assert expanded_code == "ASSIGN_MUL(VAR(a), INT(1))" 
+  assert expanded_code == "ASSIGN(VAR(a), MUL(VAR(a), INT(1)))" 
 
 def test_parser_ASSIGN_OTHER_04():
   expanded_code = get_expanded_code("a/=1;")
-  assert expanded_code == "ASSIGN_DIV(VAR(a), INT(1))" 
+  assert expanded_code == "ASSIGN(VAR(a), DIV(VAR(a), INT(1)))" 
 
 def test_parser_ASSIGN_OTHER_05():
   expanded_code = get_expanded_code("a%=1;")
-  assert expanded_code == "ASSIGN_MOD(VAR(a), INT(1))" 
+  assert expanded_code == "ASSIGN(VAR(a), MOD(VAR(a), INT(1)))" 
 
 def test_parser_ASSIGN_OTHER_06():
   expanded_code = get_expanded_code("a**=1;")
-  assert expanded_code == "ASSIGN_POW(VAR(a), INT(1))" 
+  assert expanded_code == "ASSIGN(VAR(a), POW(VAR(a), INT(1)))" 
 
 def test_parser_ASSIGN_OTHER_07():
   expanded_code = get_expanded_code("a+=1+2;")
-  assert expanded_code == "ASSIGN_ADD(VAR(a), ADD(INT(1), INT(2)))" 
+  assert expanded_code == "ASSIGN(VAR(a), ADD(VAR(a), ADD(INT(1), INT(2))))" 
 
 def test_parser_ASSIGN_OTHER_08():
   expanded_code = get_expanded_code("a-=1+2;")
-  assert expanded_code == "ASSIGN_SUB(VAR(a), ADD(INT(1), INT(2)))" 
+  assert expanded_code == "ASSIGN(VAR(a), SUB(VAR(a), ADD(INT(1), INT(2))))" 
 
 def test_parser_ASSIGN_OTHER_09():
   expanded_code = get_expanded_code("a*=1+2;")
-  assert expanded_code == "ASSIGN_MUL(VAR(a), ADD(INT(1), INT(2)))" 
+  assert expanded_code == "ASSIGN(VAR(a), MUL(VAR(a), ADD(INT(1), INT(2))))" 
 
 def test_parser_ASSIGN_OTHER_10():
   expanded_code = get_expanded_code("a/=1+2;")
-  assert expanded_code == "ASSIGN_DIV(VAR(a), ADD(INT(1), INT(2)))" 
+  assert expanded_code == "ASSIGN(VAR(a), DIV(VAR(a), ADD(INT(1), INT(2))))" 
 
 def test_parser_ASSIGN_OTHER_11():
   expanded_code = get_expanded_code("a%=1+2;")
-  assert expanded_code == "ASSIGN_MOD(VAR(a), ADD(INT(1), INT(2)))" 
+  assert expanded_code == "ASSIGN(VAR(a), MOD(VAR(a), ADD(INT(1), INT(2))))" 
 
 def test_parser_ASSIGN_OTHER_12():
   expanded_code = get_expanded_code("a**=1+2;")
-  assert expanded_code == "ASSIGN_POW(VAR(a), ADD(INT(1), INT(2)))" 
-
-def test_parser_ASSIGN_OTHER_07():
-  expanded_code = get_expanded_code("a+=1+2;")
-  assert expanded_code == "ASSIGN_ADD(VAR(a), ADD(INT(1), INT(2)))" 
+  assert expanded_code == "ASSIGN(VAR(a), POW(VAR(a), ADD(INT(1), INT(2))))" 
 
 # ---
--- Python/tori_compiler/main.py
+++ Python/tori_compiler/main.py
@@ -5,7 +5,7 @@ from tr_run import tori_minus_run
 from tr_show_tree import show_trees
 
 program = """
-a += 1+2;
+a -= 1+2;
 """
 
 tokens = tr_lexer(program)
