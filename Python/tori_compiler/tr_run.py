6d964ff17017e7ab5d074a370689326eafc93d67
yamabato <harutakutsukawa0601@gmail.com>
2023-09-29 10:05:05 +0900

    update: 評価器をクラス化

--- Python/tori_compiler/tr_run.py
+++ Python/tori_compiler/tr_run.py
@@ -12,56 +12,64 @@ ARITHMETIC_OPERATORS = [
   TR_Node_Kind.POW
 ]
 
-# ---
-# 各nodeの処理
-def eval_arithemetic_operation(node):
-  left = tr_eval(node.left)
-  right = tr_eval(node.right)
-  oper = node.kind
-
-  if oper == TR_Node_Kind.ADD: ret = left + right
-  elif oper == TR_Node_Kind.SUB: ret = left - right
-  elif oper == TR_Node_Kind.MUL: ret = left * right
-  elif oper == TR_Node_Kind.DIV: ret = left / right
-  elif oper == TR_Node_Kind.MOD: ret = left % right
-  elif oper == TR_Node_Kind.POW: ret = left ** right
-
-  return ret
-
-def eval_int(node):
-  value = node.value
-  return int(value)
-
-def eval_dec(node):
-  value = node.value
-  return float(value)
-
-def eval_str(node):
-  value = node.value
-  return value
 
 # ---
 
-def tr_eval(node):
-  node_kind = node.kind
+class Evaluator:
+  def __init__(self):
+    self.env = {}
 
-  if node_kind == TR_Node_Kind.INT:
-    ret = eval_int(node)
+  def eval(self, node):
+    node_kind = node.kind
 
-  elif node_kind == TR_Node_Kind.DEC:
-    ret = eval_dec(node)
+    if node_kind == TR_Node_Kind.INT:
+      ret = self.eval_int(node)
 
-  elif node_kind == TR_Node_Kind.STR:
-    ret = eval_str(node)
+    elif node_kind == TR_Node_Kind.DEC:
+      ret = self.eval_dec(node)
 
-  elif node_kind in ARITHMETIC_OPERATORS:
-    ret = eval_arithemetic_operation(node)
+    elif node_kind == TR_Node_Kind.STR:
+      ret = self.eval_str(node)
 
-  return ret
+    elif node_kind in ARITHMETIC_OPERATORS:
+      ret = eval_arithemetic_operation(node)
+
+    return ret
+
+  # 各ノードの評価
+
+  def eval_int(self, node):
+    value = node.value
+    return int(value)
+
+  def eval_dec(self, node):
+    value = node.value
+    return float(value)
+
+  def eval_str(self, node):
+    value = node.value
+    return value
+
+  def eval_arithemetic_operation(self, node):
+    left = self.eval(node.left)
+    right = self.eval(node.right)
+    oper = node.kind
+
+    if oper == TR_Node_Kind.ADD: ret = left + right
+    elif oper == TR_Node_Kind.SUB: ret = left - right
+    elif oper == TR_Node_Kind.MUL: ret = left * right
+    elif oper == TR_Node_Kind.DIV: ret = left / right
+    elif oper == TR_Node_Kind.MOD: ret = left % right
+    elif oper == TR_Node_Kind.POW: ret = left ** right
+
+    return ret
+
+# ---
 
 def tori_minus_run(trees):
+  evaluator = Evaluator()
   for tree in trees:
-    value = tr_eval(tree)
+    value = evaluator.eval(tree)
     print(value)
 
 # ---
