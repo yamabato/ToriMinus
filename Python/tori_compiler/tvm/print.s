fa60d856ab75c27242903ba12b247bf1fd906f40
yamabato <harutakutsukawa0601@gmail.com>
2023-10-23 00:40:13 +0900

    tvm: print.sにコメントを追記

--- Python/tori_compiler/tvm/print.s
+++ Python/tori_compiler/tvm/print.s
@@ -3,14 +3,14 @@
   .globl  print
   .type   print, @function
 tvm_print:
-  pop r10           ;rbpの値をr10に退避
+  pop r10						#スタックトップのrbpの値をr10に退避
   tvm_print_lp:
-  pop rax           
+  pop rax
   mov edi, eax
-  cmp rdi, 0        
-  je tvm_print_fin  ;次の文字が\x00なら終了
+  cmp rdi, 0
+  je tvm_print_fin	#次の文字が\x00なら終了
   call putchar@PLT
-  jmp tvm_print_lp  ;繰り返してputchar
+  jmp tvm_print_lp	#繰り返してputchar
   tvm_print_fin:
-  push r10          ;rbpの値をスタックに戻す
+  push r10					#rbpの値をスタックに戻す
   ret
